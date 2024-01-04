75421885b7e0502dfd04a26e08b7a9d28ffa0ca5
charlinleo <ccharlin@student.ciputra.ac.id>
2024-01-04 15:40:31 +0700

    5

--- ALP_webdev/resources/views/Product/index.blade.php
+++ ALP_webdev/resources/views/Product/index.blade.php
@@ -15,36 +15,43 @@
             </div>
             <div>
                 @foreach ($product as $pro)
-                    <div class="card mx-auto mb-4" style="width: 50%;">
-                        <div class="card-body justify-content-center">
-                            <h5 class="card-title text-center">{{ $pro['product_name'] }}</h5>
-                            <p class="card-text text-center">Kategori : {{ $pro->category->category_name }}</p>
-                            <p class="card-text text-center">Harga Products : {{ $pro['product_price'] }}/pcs</p>
-                            {{-- {{ dd($pro) }} --}}
-                            <p class="card-text text-center">Deskripsi : {{ $pro['product_desc'] }}</p>
-                            <p class="card-text text-center">
-                                <a href="/product" class="btn btn-primary text-center">Back</a>
-                                @if (Auth::check() && Auth::user()->isAdmin())
-                                    <a href="{{ route('product.edit', [$pro->id]) }}"
-                                        class="btn btn-warning text-white">Edit</a>
-                            </p>
-                            <p>
-                            <form action="{{ route('product.destroy', $pro->id) }}" method="POST" style="display: inline;">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger justify-center"
-                                    onclick="return confirm('Are you sure you want to delete this product?')">Delete</button>
-                            </form>
-                @endif
-                </p>
+                    <div class="">
+                        <div class="card mx-auto mb-4" style="width: 50%;">
+                            <div class="card-body justify-content-center">
+                                <h5 class="card-title text-center">{{ $pro['product_name'] }}</h5>
+                                <p class="card-text">Kategori :</p>
+                                <p class="card-text text-center">{{ $pro->category->category_name }}</p>
+                                <p class="card-text">Harga Products :</p>
+                                <p class="card-text text-center">{{ $pro['product_price'] }}/pcs</p>
+                                <p class="card-text">Deskripsi :</p>
+                                <p class="card-text text-break text-center">{{ $pro['product_desc'] }}</p>
+                                <p class="card-text ">
+                                    @if (Auth::check() && Auth::user()->isAdmin())
+                                        <form action="{{ route('product.destroy', $pro->id) }}" method="POST"
+                                            style="display: inline;">
+                                            <a href="{{ route('product.edit', [$pro->id]) }}"
+                                                class="btn btn-warning text-white">Edit</a>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger"
+                                                onclick="return confirm('Are you sure you want to delete this product?')">Delete</button>
+                                        </form>
+                                    @endif
+                                </p>
+                            </div>
+                        </div>
+                    </div>
             </div>
         </div>
         @endforeach
-        </div>
+
         @if (Auth::check() && Auth::user()->isAdmin())
             <a href="{{ route('product.create') }}" class="btn btn-success d-grid gap-2 col-6 mx-auto" style="width: 50%">Add Product</a>
         @endif
-        </div>
+<<<<<<< HEAD
+
+=======
+>>>>>>> b6575cddbf0ba7d1685d3b2ab7d51f4b7f5694a5
     </section>
 
 @endsection
