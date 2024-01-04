24af9accaa34263a09b7ddd108e02a6d580d11e7
charlinleo <ccharlin@student.ciputra.ac.id>
2024-01-04 15:37:52 +0700

    3

--- ALP_webdev/storage/framework/views/19ed9fd35f6f3cd8a545fdaa95e63248.php
+++ ALP_webdev/storage/framework/views/19ed9fd35f6f3cd8a545fdaa95e63248.php
@@ -13,36 +13,40 @@
             </div>
             <div>
                 <?php $__currentLoopData = $product; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $pro): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                    <div class="card mx-auto mb-4" style="width: 50%;">
-                        <div class="card-body justify-content-center">
-                            <h5 class="card-title text-center"><?php echo e($pro['product_name']); ?></h5>
-                            <p class="card-text text-center">Kategori : <?php echo e($pro->category->category_name); ?></p>
-                            <p class="card-text text-center">Harga Products : <?php echo e($pro['product_price']); ?>/pcs</p>
-                            
-                            <p class="card-text text-center">Deskripsi : <?php echo e($pro['product_desc']); ?></p>
-                            <p class="card-text text-center">
-                                <a href="/product" class="btn btn-primary text-center">Back</a>
-                                <?php if(Auth::check() && Auth::user()->isAdmin()): ?>
-                                    <a href="<?php echo e(route('product.edit', [$pro->id])); ?>"
-                                        class="btn btn-warning text-white">Edit</a>
-                            </p>
-                            <p>
-                            <form action="<?php echo e(route('product.destroy', $pro->id)); ?>" method="POST" style="display: inline;">
-                                <?php echo csrf_field(); ?>
-                                <?php echo method_field('DELETE'); ?>
-                                <button type="submit" class="btn btn-danger justify-center"
-                                    onclick="return confirm('Are you sure you want to delete this product?')">Delete</button>
-                            </form>
-                <?php endif; ?>
-                </p>
+                    <div class="">
+                        <div class="card mx-auto mb-4" style="width: 50%;">
+                            <div class="card-body justify-content-center">
+                                <h5 class="card-title text-center"><?php echo e($pro['product_name']); ?></h5>
+                                <p class="card-text">Kategori :</p>
+                                <p class="card-text text-center"><?php echo e($pro->category->category_name); ?></p>
+                                <p class="card-text">Harga Products :</p>
+                                <p class="card-text text-center"><?php echo e($pro['product_price']); ?>/pcs</p>
+                                <p class="card-text">Deskripsi :</p>
+                                <p class="card-text text-break text-center"><?php echo e($pro['product_desc']); ?></p>
+                                <p class="card-text ">
+                                    <?php if(Auth::check() && Auth::user()->isAdmin()): ?>
+                                        <form action="<?php echo e(route('product.destroy', $pro->id)); ?>" method="POST"
+                                            style="display: inline;">
+                                            <a href="<?php echo e(route('product.edit', [$pro->id])); ?>"
+                                                class="btn btn-warning text-white">Edit</a>
+                                            <?php echo csrf_field(); ?>
+                                            <?php echo method_field('DELETE'); ?>
+                                            <button type="submit" class="btn btn-danger"
+                                                onclick="return confirm('Are you sure you want to delete this product?')">Delete</button>
+                                        </form>
+                                    <?php endif; ?>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
             </div>
         </div>
         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-        </div>
+
         <?php if(Auth::check() && Auth::user()->isAdmin()): ?>
-            <a href="<?php echo e(route('product.create')); ?>" class="btn btn-success d-grid gap-2">Add Product</a>
+            <a href="<?php echo e(route('product.create')); ?>" class="btn btn-success d-grid gap-2 col-6 mx-auto" style="width: 50%">Add Product</a>
         <?php endif; ?>
-        </div>
+
     </section>
 
 <?php $__env->stopSection(); ?>
